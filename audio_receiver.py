44f291f759bf314d82ee71a2ea5cd4eea9980575
IsmaelRK <ismael2021ab@gmail.com>
2024-05-08 18:12:41 -0300

    features and fixes

--- audio_receiver.py
+++ audio_receiver.py
@@ -1,7 +1,15 @@
+import sys
+from pathlib import Path
+
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+import os
+import uuid
+from src.speech_recognition.speech_recognition import send_to_gemini
+
+sys.path.append(str(Path(__file__).resolve().parent.parent / "src"))
 
 app = FastAPI()
 
@@ -22,5 +30,9 @@ async def save_file(file: UploadFile = File(...)):
     if file.content_type != "audio/mpeg":
         return JSONResponse(status_code=415, content={"message": "File type not supported, use .mp3"})
 
-    with open(file.filename, "wb") as f:
-        f.write(await file.read())
+    file.filename = str(uuid.uuid4()) + ".mp3"
+
+    send_to_gemini(file)
+
+    # with open(file.filename, "wb") as f:
+    #     f.write(await file.read())
--- src/speech_recognition/speech_recognition.py
+++ src/speech_recognition/speech_recognition.py
@@ -5,74 +5,83 @@ from pathlib import Path
 import hashlib
 import google.generativeai as genai
 
-load_dotenv()
-api_key = os.getenv("API_KEY")
 
-genai.configure(api_key=api_key)
+def send_to_gemini(mp3_file):
 
-# Set up the model
-generation_config = {
-    "temperature": 1,
-    "top_p": 0.95,
-    "top_k": 0,
-    "max_output_tokens": 8192,
-}
+    load_dotenv()
+    api_key = os.getenv("API_KEY")
 
-safety_settings = [
-    {
-        "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    },
-    {
-        "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    },
-    {
-        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    },
-    {
-        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    },
-]
+    genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
-                              generation_config=generation_config,
-                              safety_settings=safety_settings)
+    generation_config = {
+        "temperature": 1,
+        "top_p": 0.95,
+        "top_k": 0,
+        "max_output_tokens": 8192,
+    }
 
-uploaded_files = []
+    safety_settings = [
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+    ]
 
+    model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
+                                  generation_config=generation_config,
+                                  safety_settings=safety_settings)
 
-def upload_if_needed(pathname: str) -> list[str]:
-    path = Path(pathname)
-    hash_id = hashlib.sha256(path.read_bytes()).hexdigest()
-    try:
-        existing_file = genai.get_file(name=hash_id)
-        return [existing_file.uri]
-    except:
-        pass
-    uploaded_files.append(genai.upload_file(path=path, display_name=hash_id))
-    return [uploaded_files[-1].uri]
+    uploaded_files = []
 
+    def upload_if_needed(pathname: str) -> list[str]:
+        path = Path(pathname)
+        hash_id = hashlib.sha256(path.read_bytes()).hexdigest()
+        try:
+            existing_file = genai.get_file(name=hash_id)
+            return [existing_file.uri]
+        except:
+            pass
+        uploaded_files.append(genai.upload_file(path=path, display_name=hash_id))
+        return [uploaded_files[-1].uri]
 
-convo = model.start_chat(history=[
-    {
-        "role": "user",
-        "parts": upload_if_needed("<path>/audio0.mp3")
-    },
-    {
-        "role": "user",
-        "parts": ["Pode transcrever o que foi falado neste audio?"]
-    },
-    {
-        "role": "model",
-        "parts": [
-            "Sim, posso transcrever o que foi falado no áudio. Você disse: \"Pode transcrever o que estou falando?\"."]
-    },
-])
+    audio_uri = upload_if_needed("./src/speech_recognition/audio0.mp3"),
 
-convo.send_message("YOUR_USER_INPUT")
-print(convo.last.text)
-for uploaded_file in uploaded_files:
-    genai.delete_file(name=uploaded_file.name)
+    convo = model.start_chat(history=[
+        {
+            "role": "user",
+            "parts": audio_uri,
+        },
+        {
+            "role": "user",
+            "parts": ["Pode transcrever o que foi falado neste audio?"]
+        },
+        {
+            "role": "model",
+            "parts": [
+                "Sim, posso transcrever o que foi falado no áudio. Você disse: \"Pode transcrever o que estou falando?\"."]
+        },
+
+        {
+            "role": "user",
+            "parts": audio_uri
+        },
+
+    ])
+
+    convo.send_message("faça o mesmo para o ultimo audio enviado")
+    print(convo.last.text)
+    for uploaded_file in uploaded_files:
+        genai.delete_file(name=uploaded_file.name)
+
