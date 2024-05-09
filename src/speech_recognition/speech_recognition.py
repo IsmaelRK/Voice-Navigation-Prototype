7dca9d1d4943152dfda1d7e04cbe898b04c92a5d
IsmaelRK <ismael2021ab@gmail.com>
2024-05-08 22:50:11 -0300

    json, need AI improvements

--- src/speech_recognition/speech_recognition.py
+++ src/speech_recognition/speech_recognition.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv
 import google.generativeai as genai
 
@@ -41,11 +42,15 @@ def send_to_gemini(mp3_file_path, mp3_file_name, speech_form_results_examples):
 
     prompt = (f"De acordo com o audio, me devolva em json as keys e informações retiradas da fala, seguem exemplos de "
               f"retorno, seja exato e devolva apenas os resultados na lingua utilizada "
-              f"no audio: {speech_form_results_examples}")
+              f"no audio: {speech_form_results_examples}. Sua saida será um json, para os preços não use a moeda "
+              f"apenas o valor")
     file_to_upload = genai.upload_file(path=mp3_file_path, display_name=mp3_file_name)
 
     response = model.generate_content([prompt, file_to_upload])
-    print(response.text)
+    print(response.text[8:-6])
 
-    print(mp3_file_name)
+    # json_data = json.loads(response.text.strip('`').strip('json').strip('\n').strip())
+    json_data = json.loads(response.text[8:-6])
+    print(json.dumps(json_data, indent=4))
+
     genai.delete_file(name=file_to_upload.name)
